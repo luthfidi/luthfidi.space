8f1b0a8190fc3bbc0bd83b61da776111c542e5ea
Luthfi <luthfihadi543@gmail.com>
2026-05-04 10:06:09 +0700

    fix build

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -22,7 +22,10 @@ export const metadata: Metadata = {
   metadataBase: new URL(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : process.env.DOMAIN || "",
+      : process.env.DOMAIN ||
+        (process.env.VERCEL_URL
+          ? `https://${process.env.VERCEL_URL}`
+          : "http://localhost:3000"),
   ),
   description: METADATA.description,
   keywords: METADATA.keyword,
